d5eadd845d8d379a84b33772719ec30cf3e91168
gbxnga <iamblizzyy@gmail.com>
2019-01-04 19:06:57 +0100

    added more bash scripts#6

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yum install -y httpd
-cd /var/www/html/php-aws-codepipeline-example
-cp .env /var/www/html
-cd /var/www/html
-rm -rf php-aws-codepipeline-example
-mkdir php-aws-codepipeline-example
-cp .env /var/www/html/php-aws-codepipeline-example
+# cd /var/www/html/php-aws-codepipeline-example
+# cp .env /var/www/html
+# cd /var/www/html
+# rm -rf php-aws-codepipeline-example
+# mkdir php-aws-codepipeline-example
+# cp .env /var/www/html/php-aws-codepipeline-example
