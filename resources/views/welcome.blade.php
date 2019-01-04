8a7f68dcbc3c628238f94e8fad9f9cab20d4b8e9
gbxnga <iamblizzyy@gmail.com>
2019-01-04 20:12:28 +0100

    added more bash scripts#9

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - Updated 2
+                    Laravel - Updated 3
                 </div>
 
                 <div class="links">
