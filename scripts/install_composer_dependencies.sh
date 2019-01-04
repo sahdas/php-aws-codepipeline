c04265d5cbaef50df2075adc70df93007f583fde
gbxnga <iamblizzyy@gmail.com>
2019-01-04 18:43:10 +0100

    added more bash scripts#2

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-yum install -y httpd
-cd /var/www/html/php-aws-codepipeline-example
-cp .env /var/www/html
-cd /var/www/html
-rm -rf php-aws-codepipeline-example
-mkdir php-aws-codepipeline-example
-cp .env /var/www/html/php-aws-codepipeline-example
 cd /var/www/html/php-aws-codepipeline-example
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
